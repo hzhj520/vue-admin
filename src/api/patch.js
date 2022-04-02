78098d6acfc198507a38f17cc3b3bf0069823479
gaogengzhi <gaogengzhi@eswin.com>
2022-04-02 17:31:30 +0800

    patch管理搜索框多个联动

--- src/api/patch.js
+++ src/api/patch.js
@@ -16,6 +16,13 @@ export function getSubjects() {
   })
 }
 
+export function getProjects() {
+  return request({
+    url: '/branch/projects',
+    method: 'get',
+  })
+}
+
 export function getOwners() {
   return request({
     url: '/patch/owners',
